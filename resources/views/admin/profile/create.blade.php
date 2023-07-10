5b655ef336fac23ced3379c1c96b2174510312e5
takano <goyataiko@gmail.com>
2023-07-10 07:27:19 +0000

    Initial commit

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,18 @@
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'profileの新規作成')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My profile</h2>
+                <p>111</p>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>profile</title>
+    </head>
+    <body>
+                <!--場所:mynews/resources/views/admin/profile-->
+
+        <h1>profile　edit画面</h1>
+        <h2>edit 機能</h2>
+        <h2>update 機能</h2>
+    </body>
+    
+</html>
