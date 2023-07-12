53e7c59a157334f1d5bb349dc0a9ca18670af9ff
takano <goyataiko@gmail.com>
2023-07-12 11:18:06 +0000

    php 08 task

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,10 @@ Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middl
 });
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin/profile')->name('admin.profile.')->middleware('auth')
-->group(function() {
-    Route::post('/create', 'add')->middleware('auth');
-    Route::get('/edit', 'edit')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::get('profile/create', 'add') ->name('profile.add');
+    Route::post('profile/create', 'create') ->name('profile.create');
+    Route::post('profile/edit', 'edit') ->name('profile.edit');
 });
 Auth::routes();
 
