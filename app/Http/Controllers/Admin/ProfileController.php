d38a2b44ddf47ad4607af0f78a84c68c1133a46c
takano <goyataiko@gmail.com>
2023-07-12 13:05:50 +0000

    php 09

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Profile;
 
 class ProfileController extends Controller
 {
@@ -15,6 +16,17 @@ class ProfileController extends Controller
 
     public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
         return redirect('admin/profile/create');
     }
 
