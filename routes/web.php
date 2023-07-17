3c33f30dad04ae1ccc418cf07d6dd52b464c6760
takano <goyataiko@gmail.com>
2023-07-17 08:48:05 +0000

    task14 完了

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,7 @@ Auth::routes();
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+use App\Http\Controllers\ProfileController as PublicNewsController;
+Route::get('/', [PublicNewsController::class, 'index'])->name('profile.index');
